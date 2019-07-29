7f62b628dc657d2ea270a141334676bcbe302c17
5hez <benbenye20@gmail.com>
2019-07-29 18:33:54 +0800

    :tada: 2048 game

---
+++ 2048/games/precache-manifest.d20dd5e1f6efe397c5518c28a9bad7fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/games/2048/robots.txt"
+  },
+  {
+    "revision": "24d98c321cacaebdac48",
+    "url": "/games/2048/js/chunk-vendors.02c3c0f8.js"
+  },
+  {
+    "revision": "fb6ee8fe46372dc5131d",
+    "url": "/games/2048/js/app.91368789.js"
+  },
+  {
+    "revision": "7d7c01d66a21c23d9e7ff21b9fa2202a",
+    "url": "/games/2048/index.html"
+  },
+  {
+    "revision": "596a8d3e6a63bfccadec9d95f18be61b",
+    "url": "/games/2048/img/2048/number.png"
+  },
+  {
+    "revision": "87b801856ca51590d8bfbc1bdca29d54",
+    "url": "/games/2048/img/2048/number.json"
+  },
+  {
+    "revision": "fb6ee8fe46372dc5131d",
+    "url": "/games/2048/css/app.b8ec2359.css"
+  }
+];
---
+++ 2048/games/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/games/2048/precache-manifest.d20dd5e1f6efe397c5518c28a9bad7fd.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "games"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
