d16939bbef7b8576cc2ae35ef00bca076f3ba0a4
5hez <benbenye20@gmail.com>
2019-08-19 17:53:26 +0800

    publish Tetris

---
+++ games/pixi-Tetris/precache-manifest.b0c70e4099f305d069fa8476788a766d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/games/pixi-Tetris/robots.txt"
+  },
+  {
+    "revision": "0767c5edf69727a721a1",
+    "url": "/games/pixi-Tetris/js/chunk-vendors.577aa911.js"
+  },
+  {
+    "revision": "7fa3d90782bdde3ec2f4",
+    "url": "/games/pixi-Tetris/js/app.2d0ee807.js"
+  },
+  {
+    "revision": "e3a081cbdda3643457f5b8f74fe5d4b2",
+    "url": "/games/pixi-Tetris/index.html"
+  },
+  {
+    "revision": "7e78d10cc89e3c434459a238464d1515",
+    "url": "/games/pixi-Tetris/img/one.7e78d10c.png"
+  },
+  {
+    "revision": "596a8d3e6a63bfccadec9d95f18be61b",
+    "url": "/games/pixi-Tetris/game-img/2048/number.png"
+  },
+  {
+    "revision": "87b801856ca51590d8bfbc1bdca29d54",
+    "url": "/games/pixi-Tetris/game-img/2048/number.json"
+  },
+  {
+    "revision": "7fa3d90782bdde3ec2f4",
+    "url": "/games/pixi-Tetris/css/app.e6e3dded.css"
+  }
+];
